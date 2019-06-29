eea682f34beedbcd9f53dc60c1a4e37f794e9d7e
dzwirlein <47755855+dzwirlein@users.noreply.github.com>
2019-06-29 16:55:08 -0500

    Working application

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -29,8 +29,10 @@ var connection = mysql.createConnection({
   var getInventory = function(){
       connection.query("SELECT * FROM products", function(err, res){
           for(var i=0; i<res.length; i++){
+              console.log(res[i].itemid+ " | " + res[i].product_name + " | "+ res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity + "\n");
 
           }
+          selectItem(res);
       })
 
   }
@@ -40,9 +42,60 @@ var connection = mysql.createConnection({
         .prompt([{
             name: "item",
             type: "input",
-            message: "What would you like to purchase?"
-        }]).then(function(answer){
-            
-        })
+            message: "What is the ID of the Item you would like to purchase?"
+        },
+        {   
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?",
+            validate: function(value){
+                if(isNaN(value)===false){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
 
-  }
+        }]).then(function(answer){
+
+            console.log('Customer has selected: \n    item= '  + answer.item + '\n    quantity = ' + answer.quantity);
+            var item = answer.item;
+            var quantity = answer.quantity;
+    
+        
+            var queryStr = 'SELECT * FROM products WHERE ?';
+    
+            connection.query(queryStr, {itemid: item}, function(err, res) {
+                if (err) {throw err;
+    
+                } else {
+                    var product = res[0];
+
+                    if (quantity <= product.stock_quantity) {
+                        console.log("The item you selected is in stock!");
+    
+                        var query = 'UPDATE products SET stock_quantity = ' + (product.stock_quantity - quantity) + ' WHERE itemid = ' + item ;
+                      
+                        connection.query(query, function(err, res) {
+                            if (err) throw err;
+    
+                            console.log('Your oder has been placed! Your total is $' + product.price * quantity);
+                            console.log("\n---------------------------------------------------------------------\n");
+    
+                            // End the database connection
+                            connection.end();
+                        })
+                    } else {
+                        console.log('Sorry, that item is not in stock');
+                        console.log('Please modify your order.');
+                        console.log("\n---------------------------------------------------------------------\n");
+    
+                       getInventory();
+                    }
+                }
+            })
+        })
+    }
+        
+
+       
